created random solver, bug in heatmap calculation

lab2/src/evaluate.py
@@ -26,18 +26,17 @@ def evaluate(x, size=20):
 def heatmap(x, size=20):
     assert np.shape(x)[-1] == size * size
     grid = np.cast[np.int_](np.reshape(x, (-1, size, size)))
+    points = np.minimum(
+    shift_r(grid) + shift_l(grid) + shift_t(grid) + shift_b(grid),
+    1 - grid
+    )
     heatmap = 1-(0.5*(shift_r(grid) + shift_l(grid) + shift_t(grid) + shift_b(grid)) + grid)[0]
-    return heatmap
+    return points.reshape(np.shape(x)).sum(-1), heatmap
 
 def visualize(x, size=20):
-    colors = heatmap(x)
+    gain, colors = heatmap(x)
     plt.imshow(colors, cmap="viridis")
-    plt.title(f"Gain value = {evaluate(x)}")
+    plt.title(f"Gain value = {gain}")
     plt.xticks(range(size))
     plt.yticks(range(size))
     plt.show()
-
-x = np.zeros(400)
-x[0] = 1
-x[23] = 1
-visualize(x)

lab2/src/random_solver.py
@@ -1,12 +1,15 @@
 import numpy as np
 from solver import Solver
+from typing import Dict, Any
 
 class RandomSolver(Solver):
-    """ A solver that draws n random points and returns the one with lowest goal function value"""
+    """ A solver that returns random solution to the problem"""
+    # note: this solver is used as initialization solver in GeneticSolver's solve method
     def __init__(self, size: int = 20):
         self.size = size
 
     def get_parameters(self) -> Dict[str, Any]:
-        return {'n': self.n}
+        return {'size': self.size}
 
-    def solve(
+    def solve(self) -> np.ndarray:
+        return np.random.randint(2, size=self.size*self.size)