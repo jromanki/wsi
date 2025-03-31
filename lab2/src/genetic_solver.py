282ba4f8d5b77727c04db1fdf496f32068a1a5a5
jromanki <romanowy123@gmail.com>
2025-04-01 01:56:18 +0200

    working population initialization

--- lab2/src/genetic_solver.py
+++ lab2/src/genetic_solver.py
@@ -5,12 +5,33 @@ from typing import Callable, Dict, Any
 class GeneticSolver(Solver):
     """ A solver that uses genetic algorythm to solve the problem with variable
     hyperparameters """
-    def __init__(self, size: int = 20):
-        self.size = size
+    def __init__(self, t_max: int, mu: int, pc: float, pm: float):
+        self.t_max = t_max
+        self.mu = mu
+        self.pc = pc
+        self.pm = pm
 
     def get_parameters(self) -> Dict[str, Any]:
-        return {'size': self.size}
+        return {'t_max': self.t_max,
+                'mu': self.mu,
+                'pc': self.pc,
+                'pm': self.pm}
 
-    def solve(self, init_func: Callable[[], np.ndarray], gain_func: Callable[[np.ndarray], int],
-            t_max: int, mu: int, pc: float, pm: float) -> np.ndarray:
+    def initialize_population(self, init_func, gain_func):
+        p, o = [], []
+        for _ in range(self.mu):
+            x = init_func()
+            p.append(x)
+            o.append(gain_func(x))
+        return p, o
+
+    def find_best(self, p, o):
+        o_max = np.max(o)
+        return p[o.index(o_max)], o_max
+
+    def solve(self, init_func: Callable[[int], np.ndarray],
+            gain_func: Callable[[np.ndarray], int],) -> np.ndarray:
         t = 0
+        p, o = self.initialize_population(init_func, gain_func)
+        p_max, o_max = self.find_best(p, o)
+        return p_max, o_max
